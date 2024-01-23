960627343571a4d6eac8f6a6191ce991b5000f8e
Matan Kushner <hello@matchai.dev>
2024-01-23 18:51:44 +0700

    Add example email with error

---
+++ emails/url-repro.tsx
@@ -0,0 +1,22 @@
+import { Body, Button, Head, Html } from "@react-email/components";
+import * as React from "react";
+
+interface Props {
+  ctaText: string;
+  ctaUrl: URL;
+}
+
+export default function UrlReproEmail({ ctaText, ctaUrl }: Props) {
+  <Html>
+    <Head />
+    <Body>
+      <Button href={ctaUrl.toString()}>{ctaText}</Button>
+    </Body>
+  </Html>;
+}
+
+UrlReproEmail.PreviewProps = {
+  ctaText: "Click me",
+  // ReferenceError: URL is not defined
+  ctaUrl: new URL("https://example.com"),
+} as Props;
